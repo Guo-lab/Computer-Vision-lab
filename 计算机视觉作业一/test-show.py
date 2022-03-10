625a44b4bc97bb0ebac8593cb39f0873df14a255
gsq <1811400940@qq.com>
2022-03-10 21:47:48 +0800

    MNIST Num Recognition Done

---
+++ 计算机视觉作业一/test-show.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+import numpy
+import matplotlib.pyplot as plt
+
+# 直接使用plt.imshow无法显示图片，需要导入pylab包
+import pylab
+
+# 打开并读取文件
+data_file = open("mnist_train.csv")
+data_list = data_file.readlines()
+data_file.close()
+
+# 拆分绘制28*28图形
+all_pixels = data_list[0].split(',')
+image_array = numpy.asfarray(all_pixels[1:]).reshape((28, 28))
+plt.imshow(image_array, cmap='gray', interpolation='None')
+pylab.show()
